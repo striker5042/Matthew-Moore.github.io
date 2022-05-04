c0e5cd377a7951dcd4e143798a0d10cc832432ca
Matthew Moore <mattmoore959@gmail.com>
2022-05-04 00:08:47 +0000

    save

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -303,6 +303,8 @@ var fibonacci = function(n) {
 var nthFibo = function(n) {
 
 
+
+
 };
 // 26. Given an array of words, return a new array containing each word capitalized.
 // var words = ['i', 'am', 'learning', 'recursion'];
@@ -347,22 +349,34 @@ var flatten = function(arrays) {
 
 // 30. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {'p':1, 'o':2, 't':2, 'a':1}
-var letterTally = function(str, obj) {
-if (str.length === 0) {
-  return obj;
+var letterTally = function(str, obj = {}) {
+  if (str.length === 0) {
+    return obj;
+  }
+  if (!obj[str[0]]) {
+  obj[str[0]] = 1;
+  }
+  else if (obj[str[0]]) {
+    obj[str[0]]++;
+  }
+   return letterTally(str.slice(1), obj);
 }
-if (obj[str].includes(obj[str[0]]))
-obj[str[0]] += 1
-
-return letterTally(str.slice(1), obj)
-};
 
 // 31. Eliminate consecutive duplicates in a list.  If the list contains repeated
 // elements they should be replaced with a single copy of the element. The order of the
 // elements should not be changed.
 // Example: compress([1, 2, 2, 3, 4, 4, 5, 5, 5]) // [1, 2, 3, 4, 5]
 // Example: compress([1, 2, 2, 3, 4, 4, 2, 5, 5, 5, 4, 4]) // [1, 2, 3, 4, 2, 5, 4]
-var compress = function(list) {
+var compress = function(list, output=[]) {
+if (list.length === 0) {
+  return output;
+}
+if (list[0] !== list[1]) {
+output.push(list[0])
+
+}
+
+return compress(list.slice(1), output)
 };
 
 // 32. Augment every element in a list with a new value where each element is an array
@@ -374,22 +388,79 @@ var augmentElements = function(array, aug) {
 // 33. Reduce a series of zeroes to a single 0.
 // minimizeZeroes([2,0,0,0,1,4]) // [2,0,1,4]
 // minimizeZeroes([2,0,0,0,1,0,0,4]) // [2,0,1,0,4]
-var minimizeZeroes = function(array) {
+var minimizeZeroes = function(array, output=[]) {
+if (array.length === 0) {
+  return output;
+}
+if (array[0] !== 0) {
+output.push(array[0])
+
+} else if (array[0] === 0 && array[1] !== 0) {
+  output.push(array[0])
+}
+
+return minimizeZeroes(array.slice(1), output)
 };
 
 // 34. Alternate the numbers in an array between positive and negative regardless of
 // their original sign.  The first number in the index always needs to be positive.
 // alternateSign([2,7,8,3,1,4]) // [2,-7,8,-3,1,-4]
 // alternateSign([-2,-7,8,3,-1,4]) // [2,-7,8,-3,1,-4]
-var alternateSign = function(array) {
+var alternateSign = function(array, output=[]) {
+  
+  if(array.length === 0){
+    return output
+  };
+  
+  if(array[0] < 0){
+    array[0] *= -1
+    output.push(array[0]);
+  } else if(array[0] > 0){
+    output.push(array[0]);
+  } if(array[1] < 0){
+    output.push(array[1]);
+  } else if(array[1] > 0){
+    array[1] *= -1;
+    output.push(array[1]);
+  }
+  return alternateSign(array.slice(2), output)
 };
 
 // 35. Given a string, return a string with digits converted to their word equivalent.
 // Assume all numbers are single digits (less than 10).
 // numToText("I have 5 dogs and 6 ponies"); // "I have five dogs and six ponies"
-var numToText = function(str) {
-};
+var numToText = function(str, output="") {
+  
+  if(str.length === 0) 
+    return output;
+    var numObj = numToText(str.substring(0, str.length - 1));
+    var replace;
+    
+    switch (str[str.length - 1]) {
+      case '1' : replace = 'one';
+      break;
+      case '2' : replace = 'two';
+      break;
+      case '3' : replace = 'three';
+      break;
+      case '4' : replace = 'four';
+      break;
+      case '5' : replace = 'five';
+      break;
+      case '6' : replace = 'six';
+      break;
+      case '7' : replace = 'seven';
+      break;
+      case '8' : replace = 'eight';
+      break;
+      case '9' : replace = 'nine';
+      break;
+      default: replace = str[str.length - 1];
+      break;
+    }
 
+    return numObj + replace;
+};
 // *** EXTRA CREDIT ***
 
 // 36. Return the number of times a tag occurs in the DOM.
@@ -407,6 +478,10 @@ var binarySearch = function(array, target, min, max) {
 // Sample array:  [34,7,23,32,5,62]
 // Sample output: [5,7,23,32,34,62]
 var mergeSort = function(array) {
+
+
+
+
 };
 
 
